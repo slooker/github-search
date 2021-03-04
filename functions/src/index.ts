4f04bca89165c5dc89da753ad34d97a5539518ce
Shawn Looker <shawn.looker@gmail.com>
2021-03-04 14:06:23 -0500

    Cleaned up some css so it looks at least halfway reasonable.  And it's responsive.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,63 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions"
+import fetch from "node-fetch"
+// I couldn't figure out how to write this Typescript style
+const cors = require("cors")({origin: true});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//   functions.logger.info("Hello logs!", {structuredData: true})
+//   response.send("Hello from Firebase!")
+// })
 
-export const searchUser = functions.https.onRequest(async (request, response) => {
-	functions.logger.info("User searched for ",request.body)
-	const url = 'https://api.github.com/search/users?q=' + encodeURIComponent(request.body)
-	functions.logger.info('Url: ', url)
-	const resp = await fetch(url)
+export const searchUser = functions.https.onRequest(
+	async (request, response) => {
+		cors(request, response, async () => {
+		// Check for POST request
+			const data = await _searchUser(request.body.search)
+			// functions.logger.info(data)
 
-	functions.logger.info('Response body: ', resp.body)
+			response.send(data)
 
-	response.send(resp.body)
+		// https://api.github.com/search/users?q=Shawn%20Looker
+		})
+	},
+)
 
-	// https://api.github.com/search/users?q=Shawn%20Looker
-})
+const _searchUser = async (search: string) => {
+	const searchUrl = "https://api.github.com/search/users?q=" + encodeURIComponent(search)
+	const searchRes = await fetch(searchUrl);
+	const searchData = await searchRes.json(); // assuming data is json
+
+	console.log("Search data")
+	console.log(searchData.items)
+	console.log("Count of search items: " + searchData.items.length)
+
+	const userList = []
+	for (let i = 0; i < searchData.items.length; i++) {
+		console.log("I: " + i)
+		const searchEntry = searchData.items[i]
+		const userUrl = "https://api.github.com/users/"+searchEntry["login"]
+		console.log("User url: " + userUrl)
+		const userRes = await fetch(userUrl)
+		const userData = await userRes.json()
+
+		userList.push({
+			username: searchEntry.login,
+			avatarUrl: searchEntry.avatar_url,
+			location: userData.location,
+			realName: userData.name,
+			// This is often null, due to privacy reasons I assume?
+			email: userData.email,
+			publicRepos: userData.public_repos,
+			accountCreationTimestamp: userData.created_at,
+			lastUpdateTimestamp: userData.updated_at,
+		})
+
+		console.log("User data")
+		console.log(userData)
+	}
+	return userList
+	// return users
+}
